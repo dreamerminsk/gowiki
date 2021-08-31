d6461e05b864e15f03e2ca56b95ea53bfd27b234
Micah Teeuws <tutmirage@gmail.com>
2021-08-31 18:25:11 +0300

    Update wiki.go

--- wiki.go
+++ wiki.go
@@ -1,1 +1,73 @@
 
+type Season struct {
+	Year int64
+	Wiki string
+}
+
+func getSeason(s *goquery.Selection) *Season {
+	var season = new(Season)
+	txt := s.Text()
+	if year, err := strconv.ParseInt(txt, 10, 32); err == nil {
+		season.Year = year
+	}
+	if ref, ok := s.Attr("href"); ok {
+		season.Wiki = ref
+	}
+	return season
+}
+
+func getATPSeasons() map[int64]*Season {
+	seasons := make(map[int64]*Season)
+
+	res, err := http.Get("https://en.wikipedia.org/wiki/Template:ATP_seasons")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find("a").FilterFunction(func(i int, s *goquery.Selection) bool {
+		txt := s.Text()
+		_, err := strconv.ParseInt(txt, 10, 64)
+		return err == nil
+	}).Each(func(i int, s *goquery.Selection) {
+		season := getSeason(s)
+		seasons[season.Year] = season
+	})
+	return seasons
+}
+
+func getWTASeasons() map[int64]*Season {
+	seasons := make(map[int64]*Season)
+
+	res, err := http.Get("https://en.wikipedia.org/wiki/Template:WTA_seasons")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find("a").FilterFunction(func(i int, s *goquery.Selection) bool {
+		txt := s.Text()
+		_, err := strconv.ParseInt(txt, 10, 64)
+		return err == nil
+	}).Each(func(i int, s *goquery.Selection) {
+		season := getSeason(s)
+		seasons[season.Year] = season
+	})
+	return seasons
+}
