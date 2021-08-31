9c3084d79b2a0e314e9c18c7afac486b0d669b71
Micah Teeuws <tutmirage@gmail.com>
2021-08-31 10:43:36 +0300

    func getWTASeasons()

--- main.go
+++ main.go
@@ -26,7 +26,9 @@ func getSeason(s *goquery.Selection) *Season {
 	return season
 }
 
-func main() {
+func getATPSeasons() map[int64]*Season {
+	seasons := make(map[int64]*Season)
+
 	res, err := http.Get("https://en.wikipedia.org/wiki/Template:ATP_seasons")
 	if err != nil {
 		log.Fatal(err)
@@ -46,7 +48,47 @@ func main() {
 		_, err := strconv.ParseInt(txt, 10, 64)
 		return err == nil
 	}).Each(func(i int, s *goquery.Selection) {
-		txt := s.Text()
-		fmt.Printf("ATP season %d: %s\n\n", i, txt)
+		season := getSeason(s)
+		seasons[season.Year] = season
 	})
+	return seasons
+}
+
+func getWTASeasons() map[int64]*Season {
+	seasons := make(map[int64]*Season)
+
+	res, err := http.Get("https://en.wikipedia.org/wiki/Template:WTA_seasons")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find("a").FilterFunction(func(i int, s *goquery.Selection) bool {
+		txt := s.Text()
+		_, err := strconv.ParseInt(txt, 10, 64)
+		return err == nil
+	}).Each(func(i int, s *goquery.Selection) {
+		season := getSeason(s)
+		seasons[season.Year] = season
+	})
+	return seasons
+}
+
+func main() {
+	atps := getATPSeasons()
+	for key, value := range atps {
+		fmt.Println("Year: ", key, "Value: ", value)
+	}
+	wtas := getWTASeasons()
+	for key, value := range wtas {
+		fmt.Println("Year: ", key, "Value: ", value)
+	}
 }
