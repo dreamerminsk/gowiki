4693f8321d97dbe61d9c205ad985c5b378d26617
Micah Teeuws <tutmirage@gmail.com>
2021-09-11 12:22:23 +0300

    const defaultUserAgent

--- web/web.go
+++ web/web.go
@@ -6,6 +6,8 @@ import (
 	"time"
 )
 
+const defaultUserAgent = "Mozilla/5.0 (Linux; Android 10; LM-X420) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Mobile Safari/537.36"
+
 type WebClient struct {
 	Client *http.Client
 }
@@ -23,15 +25,16 @@ func (wc *WebClient) Get(url string) (resp *http.Response, err error) {
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Add("User-Agent", "")
+	req.Header.Add("User-Agent", defaultUserAgent)
 	return wc.Client.Do(req)
 }
+
 func (wc *WebClient) Post(url, contentType string, body io.Reader) (resp *http.Response, err error) {
 	req, err := http.NewRequest("POST", url, body)
 	if err != nil {
 		return nil, err
 	}
 	req.Header.Set("Content-Type", contentType)
-	req.Header.Add("User-Agent", "")
+	req.Header.Add("User-Agent", defaultUserAgent)
 	return wc.Client.Do(req)
 }
