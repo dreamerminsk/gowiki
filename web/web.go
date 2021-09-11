b6dbff28d76963c7b6d7146e1bc3297b803db10e
Micah Teeuws <tutmirage@gmail.com>
2021-09-11 12:09:49 +0300

    func (wc *WebClient) Get(url string) (resp *http.Response, err error)

--- web/web.go
+++ web/web.go
@@ -16,3 +16,11 @@ func NewWebClient() *WebClient {
 		},
 	}
 }
+
+func (wc *WebClient) Get(url string) (resp *http.Response, err error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	return wc.Client.Do(req)
+}
