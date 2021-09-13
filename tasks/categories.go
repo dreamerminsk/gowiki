9fcb9c818e476ef9e5ff20b7bcd4b4c25d6e40aa
Micah Teeuws <tutmirage@gmail.com>
2021-09-13 21:13:36 +0300

    Update categories.go

--- tasks/categories.go
+++ tasks/categories.go
@@ -1,2 +1,13 @@
 package tasks
 
+
+func updateCategories() {
+        cats, err := getCategories()
+	if err != nil {
+		fmt.Println("ERROR : ", err)
+	}
+	for _, cat := range cats {
+		fmt.Println("Title: ", cat.Title)
+		g.DB.Create(cat)
+	}
+}
