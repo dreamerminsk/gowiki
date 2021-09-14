a8f778df17f3589e95975e6c675014527db1c0d7
Micah Teeuws <tutmirage@gmail.com>
2021-09-14 14:31:10 +0300

    signal.Notify

--- main.go
+++ main.go
@@ -1,15 +1,39 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/dreamerminsk/gowiki/tasks"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	tasks.UpdateCategories()
 
-	tasks.UpdateForums()
+	ticker := time.NewTicker(time.Duration(60) * time.Second)
+	defer ticker.Stop()
 
-	tasks.UpdateTopics()
+	keyChan := make(chan os.Signal, 1)
+	signal.Notify(keyChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go tasks.UpdateCategories()
+
+	go tasks.UpdateForums()
+
+	go tasks.UpdateTopics()
+
+	for {
+		select {
+		case <-keyChan:
+			fmt.Println("CTRL-C: Завершаю работу. Всего записей: ")
+			return
+		case <-ticker.C:
+			fmt.Printf("Всего  / Повторов  ( записей/сек) \n")
+
+		}
+	}
 
 }
