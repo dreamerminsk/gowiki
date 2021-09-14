8d362f7661edfac205967fb3510cdcb17bb7e691
Micah Teeuws <tutmirage@gmail.com>
2021-09-14 14:16:26 +0300

    tasks.UpdateTopics()

--- main.go
+++ main.go
@@ -1,77 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
-	"reflect"
-	"time"
-
-	"github.com/dreamerminsk/gowiki/model"
 	"github.com/dreamerminsk/gowiki/tasks"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-
 	tasks.UpdateCategories()
 
 	tasks.UpdateForums()
 
-	s, err := NewStorage()
-	if err != nil {
-		fmt.Printf("Storage: %s", err.Error())
-	}
-	defer s.Close()
-	for i := 1; i < 200; i++ {
-		processTopicPage(s, tasks.Music, i)
-		time.Sleep(RandDuration(32, 128))
-		processTopicPage(s, tasks.HDMusic, i)
-		time.Sleep(RandDuration(32, 128))
-		processTopicPage(s, tasks.MusicCollections, i)
-		time.Sleep(RandDuration(32, 128))
-		processTopicPage(s, tasks.AnimeAndManga, i)
-		time.Sleep(RandDuration(32, 128))
-		processTopicPage(s, tasks.BooksAndMediaMaterials, i)
-		time.Sleep(RandDuration(32, 128))
-	}
+	tasks.UpdateTopics()
 
-}
-
-func processTopicPage(s *SqliteStorage, catID tasks.NnmClubCategory, page int) {
-	topics := tasks.GetTopics(catID, page)
-	for key, topic := range topics {
-		fmt.Println("ID: ", key)
-		fmt.Println("Title: ", topic.Title)
-		fmt.Println("Author: ", topic.Author)
-		fmt.Println("Published: ", topic.Published.Format(time.RFC3339))
-		fmt.Println("Likes: ", topic.Likes)
-		fmt.Println("Magnet: ", topic.Magnet)
-		insertOrUpdate(s, topic)
-		fmt.Println("-------------------------")
-	}
-}
-
-func insertOrUpdate(s *SqliteStorage, topic *model.Topic) {
-	oldTopic, selectErr := s.getTopic(int(topic.ID))
-	if selectErr != nil {
-		fmt.Println("SELECT ERROR: ", reflect.TypeOf(selectErr), selectErr)
-	}
-	if oldTopic.ID == 0 {
-		insertErr := s.addTopic(topic)
-		if insertErr != nil {
-			fmt.Println("INSERT ERROR: ", reflect.TypeOf(insertErr), insertErr)
-		}
-	} else if topic.Likes > oldTopic.Likes {
-		fmt.Printf("\tDIFF LIKES: %d\r\n", topic.Likes-oldTopic.Likes)
-		updateErr := s.updateTopic(topic)
-		if updateErr != nil {
-			fmt.Println("UPDATE ERROR: ", reflect.TypeOf(updateErr), updateErr)
-		}
-	}
-}
-
-func RandDuration(min, max int) time.Duration {
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	return time.Second * time.Duration((r.Intn(max-min) + min))
 }
