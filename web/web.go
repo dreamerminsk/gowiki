dbf82b4816c74f0a8d1d71af2139b4859803834e
Micah Teeuws <tutmirage@gmail.com>
2021-09-16 07:01:18 +0300

    Update web.go

--- web/web.go
+++ web/web.go
@@ -2,39 +2,46 @@ package web
 
 import (
 	"io"
+"sync"
 	"net/http"
 	"time"
 )
 
 const defaultUserAgent = "Mozilla/5.0 (Linux; Android 10; LM-X420) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Mobile Safari/537.36"
 
-type WebClient struct {
-	Client *http.Client
+type webClient struct {
+	client *http.Client
+        mu sync.Mutex
 }
 
-func NewWebClient() *WebClient {
-	return &WebClient{
-		Client: &http.Client{
+type WebReader interface{
+Get(url string) (resp *http.Response, err error)
+Post(url, contentType string, body io.Reader) (resp *http.Response, err error)
+}
+
+func New() *WebReader {
+	return &webClient{
+		client: &http.Client{
 			Timeout: time.Second * 60,
 		},
 	}
 }
 
-func (wc *WebClient) Get(url string) (resp *http.Response, err error) {
+func (wc *webClient) Get(url string) (resp *http.Response, err error) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
 	req.Header.Add("User-Agent", defaultUserAgent)
-	return wc.Client.Do(req)
+	return wc.client.Do(req)
 }
 
-func (wc *WebClient) Post(url, contentType string, body io.Reader) (resp *http.Response, err error) {
+func (wc *webClient) Post(url, contentType string, body io.Reader) (resp *http.Response, err error) {
 	req, err := http.NewRequest("POST", url, body)
 	if err != nil {
 		return nil, err
 	}
 	req.Header.Set("Content-Type", contentType)
 	req.Header.Add("User-Agent", defaultUserAgent)
-	return wc.Client.Do(req)
+	return wc.client.Do(req)
 }
