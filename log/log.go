128900a2b657339babeaa5091253530693fb1ef3
Micah Teeuws <tutmirage@gmail.com>
2021-09-20 11:03:18 +0300

    Update log.go

--- log/log.go
+++ log/log.go
@@ -1,6 +1,11 @@
 package log
 
+import (
+"fmt"
+)
+
 const timeFormat = "2006-01-02T15:04:05"
 
-func log() {
+func log(msg string) {
+        fmt.Printf("[%s] [%s]\r\n\t%s\r\n", time.Now().Format(timeFormat), "webClient->Get", msg)
 }
