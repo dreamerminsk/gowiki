59cc0e4c1fb484aa2df4e0e1917c89830bd5a75b
Micah Teeuws <tutmirage@gmail.com>
2021-09-20 11:35:11 +0300

    Update log.go
    runtime.FuncForPC(pc).Name()

--- log/log.go
+++ log/log.go
@@ -2,10 +2,13 @@ package log
 
 import (
 "fmt"
+"runtime"
 )
 
 const timeFormat = "2006-01-02T15:04:05"
 
 func log(msg string) {
-        fmt.Printf("[%s] [%s]\r\n\t%s\r\n", time.Now().Format(timeFormat), "webClient->Get", msg)
+        pc, _, _, _ := runtime.Caller(1)
+    funcName := runtime.FuncForPC(pc).Name()
+        fmt.Printf("[%s] [%s]\r\n\t%s\r\n", time.Now().Format(timeFormat), funcName, msg)
 }
