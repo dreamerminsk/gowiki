8adf1efd72260f854325444246cf4bcf7a810c8e
Micah Teeuws <tutmirage@gmail.com>
2021-09-20 14:15:10 +0300

    Update log.go

--- log/log.go
+++ log/log.go
@@ -8,7 +8,7 @@ import (
 
 const timeFormat = "2006-01-02T15:04:05"
 
-func log(msg string) {
+func Log(msg string) {
         pc, _, _, _ := runtime.Caller(1)
     funcName := runtime.FuncForPC(pc).Name()
         fmt.Printf("[%s] [%s]\r\n\t%s\r\n", time.Now().Format(timeFormat), funcName, msg)
