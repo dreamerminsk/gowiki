2e4b02d61b0537d91896462a761aecc101298abd
Micah Teeuws <tutmirage@gmail.com>
2021-09-24 22:13:42 +0300

    Update utils.go
    func OrDone(done, c <-chan interface{}) <-chan interface{}

--- utils/utils.go
+++ utils/utils.go
@@ -31,3 +31,28 @@ func Or(channels ...<-chan interface{}) <-chan interface{} {
     }()
     return orDone
 }
+
+
+
+
+func OrDone(done, c <-chan interface{}) <-chan interface{} {
+    valStream := make(chan interface{})
+    go func() {
+        defer close(valStream)
+        for {
+            select {
+            case <-done:
+                return
+            case v, ok := <-c:
+                if ok == false {
+                    return
+                }
+                select {
+                case valStream <- v:
+                case <-done:
+                }
+            }
+        }
+    }()
+    return valStream
+}
