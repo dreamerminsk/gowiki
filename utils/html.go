bd5fb3120ace06ee75b9c696a2fdd23dae5edf85
Micah Teeuws <tutmirage@gmail.com>
2021-09-25 21:04:59 +0000

    go fmt utils/*.go//

--- utils/html.go
+++ utils/html.go
@@ -1,20 +1,17 @@
 package utils
+
 import (
-"net/url"
+	"net/url"
 )
 
-
-
-
-
-func GetParam(ref, name string)(*string, error) {
-    u, err := url.Parse(ref)
-    if err != nil {
-        return nil, err
-    }
-    q, err := url.ParseQuery(u.RawQuery)
-    if err != nil {
-        return nil, err
-    }
-    return &q[name][0], nil
+func GetParam(ref, name string) (*string, error) {
+	u, err := url.Parse(ref)
+	if err != nil {
+		return nil, err
+	}
+	q, err := url.ParseQuery(u.RawQuery)
+	if err != nil {
+		return nil, err
+	}
+	return &q[name][0], nil
 }
