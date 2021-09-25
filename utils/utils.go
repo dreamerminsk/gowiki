14414ab2f86ce89f798d1b0b8e1b57f977dc70d7
Micah Teeuws <tutmirage@gmail.com>
2021-09-25 20:32:39 +0000

    go fmt utils/*.go

--- utils/utils.go
+++ utils/utils.go
@@ -1,58 +1,53 @@
 package utils
 
-
-
 func Or(channels ...<-chan interface{}) <-chan interface{} {
-    switch len(channels) {
-    case 0:
-        return nil
-    case 1:
-        return channels[0]
-    }
+	switch len(channels) {
+	case 0:
+		return nil
+	case 1:
+		return channels[0]
+	}
 
-    orDone := make(chan interface{})
-    go func() {
-        defer close(orDone)
+	orDone := make(chan interface{})
+	go func() {
+		defer close(orDone)
 
-        switch len(channels) {
-        case 2:
-            select {
-            case <-channels[0]:
-            case <-channels[1]:
-            }
-        default:
-            select {
-            case <-channels[0]:
-            case <-channels[1]:
-            case <-channels[2]:
-            case <-Or(append(channels[3:], orDone)...):
-            }
-        }
-    }()
-    return orDone
+		switch len(channels) {
+		case 2:
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			}
+		default:
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			case <-channels[2]:
+			case <-Or(append(channels[3:], orDone)...):
+			}
+		}
+	}()
+	return orDone
 }
 
-
-
-
 func OrDone(done, c <-chan interface{}) <-chan interface{} {
-    valStream := make(chan interface{})
-    go func() {
-        defer close(valStream)
-        for {
-            select {
-            case <-done:
-                return
-            case v, ok := <-c:
-                if ok == false {
-                    return
-                }
-                select {
-                case valStream <- v:
-                case <-done:
-                }
-            }
-        }
-    }()
-    return valStream
+	valStream := make(chan interface{})
+	go func() {
+		defer close(valStream)
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-c:
+				if ok == false {
+					return
+				}
+				select {
+				case valStream <- v:
+				case <-done:
+				}
+			}
+		}
+	}()
+	return valStream
 }
