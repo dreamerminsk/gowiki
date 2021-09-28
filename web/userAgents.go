9358f73d3ab0ec2d442bf1aedad315309044658a
Micah Teeuws <tutmirage@gmail.com>
2021-09-28 22:29:57 +0300

    Update userAgents.go

--- web/userAgents.go
+++ web/userAgents.go
@@ -2,12 +2,18 @@ package web
 
 
 
+import(
+"time"
+"math/rand"
+)
 
 
 
 
 
 
+var uaRand        *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+)
 
 
 var userAgents = [...]string{
@@ -15,4 +21,11 @@ var userAgents = [...]string{
 "Mozilla/5.0 (iPad; CPU OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/71.0.3578.77 Mobile/15E148 Safari/605.1",
 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763",
 "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko)",
+}
+
+
+
+
+func randomUserAgent()string{
+return userAgents[uaRand.Intn(len(userAgents)]
 }
