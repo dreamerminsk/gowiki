9b044ae61dcff7771de4be01e1b041c851481bf7
Micah Teeuws <tutmirage@gmail.com>
2021-09-28 07:28:11 +0300

    Update log.go
    func getShortName(f interface{}) string

--- log/log.go
+++ log/log.go
@@ -3,6 +3,7 @@ package log
 import (
 	"fmt"
 	"runtime"
+"reflect"
 	"time"
 )
 
@@ -10,6 +11,14 @@ const timeFormat = "2006-01-02 15:04:05"
 
 func Log(msg string) {
 	pc, _, _, _ := runtime.Caller(1)
-	funcName := runtime.FuncForPC(pc).Name()
+	funcName := getShortName(pc)
 	fmt.Printf("[%s] [%s]\r\n\t%s\r\n", time.Now().Format(timeFormat), funcName, msg)
+}
+
+
+
+
+func getShortName(f interface{}) string {
+	fullName := runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name()
+	return fullName[strings.LastIndex(fullName, "/")+1:]
 }
