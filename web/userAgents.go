384ca7f8b5c81cc08da4b0117f41adad8c0ac413
Micah Teeuws <tutmirage@gmail.com>
2021-09-28 22:08:47 +0300

    Create userAgents.go

---
+++ web/userAgents.go
@@ -0,0 +1,15 @@
+package web
+
+
+
+
+
+
+
+
+
+
+
+var userAgents = [...]     {
+"Mozilla/5.0 (Linux; Android 10; LM-X420) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Mobile Safari/537.36",
+}
