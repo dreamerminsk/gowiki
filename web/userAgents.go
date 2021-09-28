148c539c6175accb52dd9d395bac121061e91343
Micah Teeuws <tutmirage@gmail.com>
2021-09-28 22:21:48 +0300

    Update userAgents.go

--- web/userAgents.go
+++ web/userAgents.go
@@ -10,7 +10,7 @@ package web
 
 
 
-var userAgents := [...]{
+var userAgents = [...]string{
 "Mozilla/5.0 (Linux; Android 10; LM-X420) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Mobile Safari/537.36",
 "Mozilla/5.0 (iPad; CPU OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/71.0.3578.77 Mobile/15E148 Safari/605.1",
 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763",
