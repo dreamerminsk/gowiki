726a511c9ef939138353e5a88881ce46976347cd
Micah Teeuws <tutmirage@gmail.com>
2021-10-02 10:49:21 +0300

    log.Logf("ERROR : %s", err)

--- tasks/forums.go
+++ tasks/forums.go
@@ -3,9 +3,8 @@ package tasks
 import (
 	"context"
 	"errors"
-	"fmt"
-	"time"
 
+	"github.com/dreamerminsk/gowiki/log"
 	"github.com/dreamerminsk/gowiki/storage"
 	"github.com/dreamerminsk/gowiki/web/nnmclub"
 	"gorm.io/gorm"
@@ -14,14 +13,14 @@ import (
 func InitForums(ctx context.Context) {
 	forums, err := nnmclub.GetForums(ctx)
 	if err != nil {
-		fmt.Println("ERROR : ", err)
+		log.Logf("ERROR : %s", err)
 	}
 	g := storage.New()
 	for _, forum := range forums {
 		if _, err := g.GetForumByID(forum.ID); err != nil {
 			if errors.Is(err, gorm.ErrRecordNotFound) {
 				g.Create(forum)
-				fmt.Println("INSERT FORUM: ", forum.ID, " - ", forum.Title)
+				log.Logf("INSERT FORUM: %d - %s", forum.ID, forum.Title)
 			}
 		}
 	}
@@ -31,13 +30,13 @@ func UpdateForums(ctx context.Context) {
 	g := storage.New()
 	forums, err := g.GetForums()
 	if err != nil {
-		fmt.Println("ERROR : ", err)
+		log.Logf("ERROR : %s", err)
 	}
 	for _, forum := range forums {
 		if forum.CatID == 0 {
 			newForum, err := nnmclub.GetForum(ctx, forum.ID)
 			if err != nil {
-				fmt.Printf("[%s] [%s] %s\r\n", time.Now().Format(time.RFC3339), "tasks->UpdateForums", err)
+				log.Logf("ERROR : %s", err)
 
 			}
 			g.UpdateForum(newForum)
