029f29aefb6de0a83c4f6fe2e16b88e87349b254
Micah Teeuws <tutmirage@gmail.com>
2021-10-10 11:06:04 +0300

    Update times.go

--- utils/times.go
+++ utils/times.go
@@ -62,11 +62,13 @@ var timeRegEx = regexp.MustCompile(timePattern)
 
 func ParseTime(timeString string) time.Time {
 	match := timeRegEx.FindStringSubmatch(timeString)
+	
 	d, _ := strconv.Atoi(match[1])
 	y, _ := strconv.Atoi(match[3])
 	h, _ := strconv.Atoi(match[4])
 	m, _ := strconv.Atoi(match[5])
 	s, _ := strconv.Atoi(match[6])
+	
 	return time.Date(
 		y, time.Month(Find(shortMonthNames, match[2])+1), d,
 		h, m, s, 0, time.UTC)
