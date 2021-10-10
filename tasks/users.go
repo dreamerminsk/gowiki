7f3b855744c32e9f0501b69b89bf5b6971cd4661
Micah Teeuws <tutmirage@gmail.com>
2021-10-10 11:29:41 +0300

    lastUser

--- tasks/users.go
+++ tasks/users.go
@@ -7,21 +7,26 @@ import (
 	"time"
 
 	"github.com/dreamerminsk/gowiki/log"
+	"github.com/dreamerminsk/gowiki/model"
 	"github.com/dreamerminsk/gowiki/storage"
 	"github.com/dreamerminsk/gowiki/web/nnmclub"
 )
 
 func InitUsers(ctx context.Context, t *Task) {
 	g := storage.New()
+
 	forums, err := g.GetForums()
 	if err != nil {
 		log.Logf("ERROR : %s", err)
 	}
+
 	rand.Seed(time.Now().UnixNano())
 	rand.Shuffle(len(forums), func(i, j int) {
 		forums[i], forums[j] = forums[j], forums[i]
 	})
+
 	newUsers := 0
+	lastUser := &model.User{}
 	for idx, forum := range forums {
 		users, err := nnmclub.GetForumUsers2(ctx, forum.ID)
 		if err != nil {
@@ -35,12 +40,13 @@ func InitUsers(ctx context.Context, t *Task) {
 
 			} else {
 				newUsers++
+				lastUser = user
 				log.Logf("INSERT USER: %d - %s", user.ID, user.Name)
 			}
 			//}
 			//}
-			t.MsgChan <- fmt.Sprintf("forums: %d of %d, new users: %d",
-				idx + 1, len(forums), newUsers)
+			t.MsgChan <- fmt.Sprintf("forums: %d of %d, new users: %d, last user: %d, %s",
+				idx+1, len(forums), newUsers, lastUser.ID, lastUser.Name)
 		}
 	}
 }
