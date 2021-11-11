247be5821e984d3a32a37855614658a79d833128
Micah Teeuws <tutmirage@gmail.com>
2021-11-11 22:56:00 +0300

    Update userAgents.go

--- web/userAgents.go
+++ web/userAgents.go
@@ -14,6 +14,7 @@ var userAgents = [...]string{
 	"Mozilla/5.0 (Macintosh; Intel Mac OS X 12_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36",
         "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36",
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 Vivaldi/4.3",
 }
 
 func randomUserAgent() string {
